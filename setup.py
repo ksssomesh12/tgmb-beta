5b35c11664669c81643287b08cd8a5eea97c907e
SomesH S <ksssomesh12@gmail.com>
2022-01-11 09:54:58 +0530

    add pyproject.toml, setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="tgmb",
+    version="0.0.0",
+    author="SomesH S",
+    author_email="ksssomesh12@gmail.com",
+    description="Predecessor to `tgmb`",
+    long_description=open('README.md', 'rt', encoding='utf-8').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ksssomesh12/tgmb-beta",
+    project_urls={
+        "Bug Tracker": "https://github.com/ksssomesh12/tgmb-beta/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Development Status :: 4 - Beta"
+    ],
+    packages=setuptools.find_packages(),
+    install_requires=open('requirements.txt', 'rt', encoding='utf-8').read().split('\n'),
+    python_requires=">=3.8"
+)
